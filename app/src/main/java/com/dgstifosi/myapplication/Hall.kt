17e2ccd68e123c7025afa84befd6773a1dc37bdb
dani891 <daniel_sanandreas@hotmail.com>
2024-11-23 16:20:10 +0100

    Version2

--- app/src/main/java/com/dgstifosi/myapplication/Hall.kt
+++ app/src/main/java/com/dgstifosi/myapplication/Hall.kt
@@ -3,6 +3,7 @@ package com.dgstifosi.myapplication
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,6 +17,16 @@ class Hall : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        // Verificar si el usuario está autenticado
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser == null) {
+            // Si no hay usuario autenticado, redirigir al login
+            val intent = Intent(this, Entrada::class.java)
+            startActivity(intent)
+            finish() // Asegura que no pueda regresar a esta actividad sin estar autenticado
+            return
+        }
         setContentView(R.layout.activity_hall)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -26,11 +37,16 @@ class Hall : AppCompatActivity(){
         butonplan.setOnClickListener { // Acción al hacer clic
             val intent = Intent(this, Navegador::class.java)
             startActivity(intent)
-
         }
-        // Botón de cerrar sesión
-        val botonLogout = findViewById<Button>(R.id.butclose)
-        botonLogout.setOnClickListener {
+            val botonperfil:ImageButton = findViewById<ImageButton>(com.dgstifosi.myapplication.R.id.ibperfil)
+            botonperfil.setOnClickListener {
+            val intent = Intent(this, Perfil::class.java)
+             startActivity(intent)
+            }
+
+            // Botón de cerrar sesión
+            val botonLogout = findViewById<Button>(R.id.butclose)
+            botonLogout.setOnClickListener {
             logOutUser() // Llama a la función que cierra sesión
         }
     }
--- app/src/main/java/com/dgstifosi/myapplication/Perfil.kt
+++ app/src/main/java/com/dgstifosi/myapplication/Perfil.kt
@@ -1,15 +1,26 @@
 package com.dgstifosi.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class Perfil : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        // Verificar si el usuario está autenticado
+        val currentUser = FirebaseAuth.getInstance().currentUser
+        if (currentUser == null) {
+            // Si no hay usuario autenticado, redirigir al login
+            val intent = Intent(this, Entrada::class.java)
+            startActivity(intent)
+            finish() // Asegura que no pueda regresar a esta actividad sin estar autenticado
+            return
+        }
         setContentView(R.layout.activity_perfil)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
